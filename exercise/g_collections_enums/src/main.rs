371e517289e26c0fe975334e32001525c4970306
bensullivan2002 <bensullivan2002@gmail.com>
2024-05-29 23:18:17 +0100

    Exercises completed

--- exercise/g_collections_enums/src/main.rs
+++ exercise/g_collections_enums/src/main.rs
@@ -11,6 +11,81 @@
 // You will need to complete 1b as well before you are able to run this program successfully.
 
 
+use std::ops::Add;
+
+fn main() {
+    // Simulate shooting a bunch of arrows and gathering their coordinates on the target.
+    let arrow_coords: Vec<Coord> = get_arrow_coords(5);
+    let mut shots: Vec<Shot> = Vec::new();
+
+    // 2. For each coord in arrow_coords:
+    //
+    //   A. Call `coord.print_description()`
+    //   B. Append the correct variant of `Shot` to the `shots` vector depending on the value of
+    //   `coord.distance_from_center()`
+    //      - Less than 1.0 -- `Shot::Bullseye`
+    //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
+    //      - Greater than 5.0 -- `Shot::Miss`
+
+    for coord in arrow_coords {
+        coord.print_description();
+        let distance = coord.distance_from_center();
+        let shot = if distance < 1.0 {
+            Shot::Bullseye
+        }
+        else if distance >= 1.0 && distance <= 5.0 {
+            Shot::Hit(distance)
+        }
+        else {
+            Shot::Miss
+        };
+        shots.push(shot);
+    }
+
+
+    let mut total = 0;
+    // 3. Finally, loop through each shot in shots and add its points to total
+
+    for shot in shots {
+        total += shot.points();
+    }
+
+    println!("Final point total is: {}", total);
+}
+
+// A coordinate of where an Arrow hit
+#[derive(Debug)]
+struct Coord {
+    x: f64,
+    y: f64,
+}
+
+impl Coord {
+    fn distance_from_center(&self) -> f64 {
+        (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
+    }
+    fn print_description(&self) {
+        println!(
+            "coord is {:.1} away, at ({:.1}, {:.1})",
+            self.distance_from_center(),
+            self.x,
+            self.y);
+    }
+}
+
+// Generate some random coordinates
+fn get_arrow_coords(num: u32) -> Vec<Coord> {
+    let mut coords: Vec<Coord> = Vec::new();
+    for _ in 0..num {
+        let coord = Coord {
+            x: (rand::random::<f64>() - 0.5) * 12.0,
+            y: (rand::random::<f64>() - 0.5) * 12.0,
+        };
+        coords.push(coord);
+    }
+    coords
+}
+
 enum Shot {
     Bullseye,
     Hit(f64),
@@ -38,59 +113,4 @@ impl Shot {
             Shot::Miss => 0,
         }
     }
-}
-
-fn main() {
-    // Simulate shooting a bunch of arrows and gathering their coordinates on the target.
-    let arrow_coords: Vec<Coord> = get_arrow_coords(5);
-    let mut shots: Vec<Shot> = Vec::new();
-
-    // 2. For each coord in arrow_coords:
-    //
-    //   A. Call `coord.print_description()`
-    //   B. Append the correct variant of `Shot` to the `shots` vector depending on the value of
-    //   `coord.distance_from_center()`
-    //      - Less than 1.0 -- `Shot::Bullseye`
-    //      - Between 1.0 and 5.0 -- `Shot::Hit(value)`
-    //      - Greater than 5.0 -- `Shot::Miss`
-
-
-    let mut total = 0;
-    // 3. Finally, loop through each shot in shots and add its points to total
-
-    println!("Final point total is: {}", total);
-}
-
-// A coordinate of where an Arrow hit
-#[derive(Debug)]
-struct Coord {
-    x: f64,
-    y: f64,
-}
-
-impl Coord {
-    fn distance_from_center(&self) -> f64 {
-        (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
-    }
-    fn print_description(&self) {
-        println!(
-            "coord is {:.1} away, at ({:.1}, {:.1})",
-            self.distance_from_center(),
-            self.x,
-            self.y);
-    }
-
-}
-
-// Generate some random coordinates
-fn get_arrow_coords(num: u32) -> Vec<Coord> {
-    let mut coords: Vec<Coord> = Vec::new();
-    for _ in 0..num {
-        let coord = Coord {
-            x: (rand::random::<f64>() - 0.5) * 12.0,
-            y: (rand::random::<f64>() - 0.5) * 12.0,
-        };
-        coords.push(coord);
-    }
-    coords
 }
